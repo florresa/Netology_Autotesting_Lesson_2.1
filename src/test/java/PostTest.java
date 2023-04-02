3b34f1db22dd8d51ae8a0c397f876ce0ca0c4452
Olga <olakostina31@gmail.com>
2023-04-02 11:54:21 +0300

    Checking build failure if tests fail

--- src/test/java/PostTest.java
+++ src/test/java/PostTest.java
@@ -13,7 +13,7 @@ public class PostTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("some value"))
         ;
     }
 }
